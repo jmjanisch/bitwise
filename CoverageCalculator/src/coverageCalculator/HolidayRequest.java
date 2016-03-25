40de6f32d860096ec805ab710b8098187c42fe5d
Justin Janisch <jjanisch@charter.net>
2016-03-25 18:15:51 -0500

    Added Holiday Request POJO

---
+++ CoverageCalculator/src/coverageCalculator/HolidayRequest.java
@@ -0,0 +1,44 @@
+package coverageCalculator;
+
+import java.util.*;
+/**
+ * Created by Justin Janisch on 3/25/2016.
+ */
+public class HolidayRequest {
+
+    private List<String> holidayRequests;
+    private int employeeId;
+
+    public HolidayRequest() {
+
+    }
+
+    public HolidayRequest(List<String> holidayRequests, int employeeId) {
+        this.holidayRequests = holidayRequests;
+        this.employeeId = employeeId;
+    }
+
+    public List<String> getHolidayRequests() {
+        return holidayRequests;
+    }
+
+    public void setHolidayRequests(List<String> holidayRequests) {
+        this.holidayRequests = holidayRequests;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public String toString() {
+        return "HolidayRequest{" +
+                "holidayRequests=" + holidayRequests +
+                ", employeeId=" + employeeId +
+                '}';
+    }
+}
