a5d2197a87adc6e1cdb7dc2d10ba45157863de93
Lora <lbahr@madisoncollege.edu>
2016-03-29 20:00:47 -0500

    put in basics of project plan added a project with maven and some dependencies

---
+++ ProjectPlan.md
@@ -0,0 +1,27 @@
+Project Plan
+
+week 1:
+create project
+create request object
+create project plan
+put maven into project
+
+week 2:
+work on parsing request with jersey
+work on logic for sending response
+
+
+week 3:
+deploy on openshift
+
+
+
+Deliverables
+	-Problem Statement
+	-project objectives
+	-project plan
+	-Journal/blog
+	-source code
+	-documentation
+	-host on opensift
+	-presentation
---
+++ holidayCalculator/src/main/coverageCalculator/persistence/RequestDaoWithHibernate.java
@@ -0,0 +1,85 @@
+package coverageCalculator.persistence;
+
+import coverageCalculator.RequestEntity;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+/**
+ * Created by Justin Janisch on 3/26/2016.
+ */
+public class RequestDaoWithHibernate {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    public List<RequestEntity> getAllRequests() {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        List<RequestEntity> allHolidayRequests = new ArrayList<RequestEntity>();
+        Transaction dbTransaction = null;
+
+        try {
+            dbTransaction = session.beginTransaction();
+            List requestsInDB = session.createQuery("FROM RequestEntity").list();
+
+            for (Iterator iterator = requestsInDB.iterator(); iterator.hasNext();) {
+
+                RequestEntity holidayRequest = (RequestEntity) iterator.next();
+                allHolidayRequests.add(holidayRequest);
+            }
+
+            dbTransaction.commit();
+
+        } catch (HibernateException error) {
+            if (dbTransaction!=null) dbTransaction.rollback();
+            error.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        log.info("Number of holiday requests: " + allHolidayRequests.size());
+
+        return allHolidayRequests;
+    }
+
+    public void updateRequest(RequestEntity requestEntity) {
+
+    }
+
+
+    public void deleteRequest(RequestEntity requestEntity) {
+
+    }
+
+
+    public int addRequest(RequestEntity requestEntity) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = null;
+        Integer requestId = null;
+
+        try {
+            transaction = session.beginTransaction();
+            requestId = (Integer) session.save(requestEntity);
+
+            session.save(requestEntity);
+            transaction.commit();
+
+            log.info("Added Request: " + requestEntity + " with id of: " + requestId);
+
+        } catch (HibernateException e) {
+            if (transaction != null) transaction.rollback();
+            log.error(e);
+
+        } finally {
+            session.close();
+        }
+        return requestId;
+    }
+
+}
